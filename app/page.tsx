3e30cb5b86aeb9477308475412784526d8177fbf
Victor <soujvnunes@gmail.com>
2024-05-29 16:19:33 -0300

    feat(home) implement contributions

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import { BlogPosts } from 'app/components/posts'
+export default async function Page() {
+  const { getContributions } = await import("actions/getContributions");
+  const { default: buildContributions } = await import(
+    "lib/buildContributions"
+  );
+  const rawContributions = await getContributions();
+  const contributions = buildContributions(rawContributions);
 
-export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      {contributions.map((contribution) => (
+        <article key={contribution.id}>
+          <h3>{contribution.name}</h3>
+        </article>
+      ))}
     </section>
-  )
+  );
 }
