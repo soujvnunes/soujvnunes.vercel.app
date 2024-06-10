f25e9a180656a63c6a89aed8b82ccc2fb827ab85
Victor <soujvnunes@gmail.com>
2024-06-10 17:07:45 +0100

    feat(index/layout) adjust theme viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
 import "./global.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Navbar } from "components/nav";
 
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#100A01" },
+    { media: "(prefers-color-scheme: light)", color: "#FFFBF4" },
+  ],
+};
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
   description: process.env.NEXT_PUBLIC_META_DESCRIPTION!,
-  themeColor: [
-    {
-      media: "(prefers-color-scheme: dark)",
-      color: "#100A01",
-    },
-    {
-      media: "(prefers-color-scheme: light)",
-      color: "#FFFBF4",
-    },
-  ],
   title: {
     default: process.env.NEXT_PUBLIC_META_TITLE!,
     template: `%s | ${process.env.NEXT_PUBLIC_META_TITLE!}`,
