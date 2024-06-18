9ff73ede15201d8e01335984c1cb50521f8295bf
Victor <soujvnunes@gmail.com>
2024-06-18 18:34:20 +0100

    feat(next.config) setup url-loader + svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  // ref: https://github.com/vercel/next.js/discussions/52690?sort=new#discussioncomment-8235460
+  webpack: (config) => {
+    // Add rule for SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
