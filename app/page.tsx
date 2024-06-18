498c6a2860378f150fb27d628ad9644c9fcb3d00
Victor <soujvnunes@gmail.com>
2024-06-18 18:43:55 +0100

    feat(home) implement Business

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-export default async function Page() {
-  const { getContributions } = await import("actions/getContributions");
-  const { default: buildContributions } = await import(
-    "lib/buildContributions"
-  );
-  const rawContributions = await getContributions();
-  const contributions = buildContributions(rawContributions);
+import Business from "components/Business";
 
-  return (
-    <section>
-      {contributions.map((contribution) => (
-        <article key={contribution.id}>
-          <h3>{contribution.name}</h3>
-        </article>
-      ))}
-    </section>
-  );
+export default function Contributions() {
+  return <Business />;
 }
