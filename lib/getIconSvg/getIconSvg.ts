c0efef5d146c16fc9f022f506c89caa1950f0f02
Victor <soujvnunes@gmail.com>
2024-06-20 18:09:57 +0100

    feat(lib/getIconSvg) adjust based on icons at public folder

--- lib/getIconSvg/getIconSvg.ts
+++ lib/getIconSvg/getIconSvg.ts
@@ -1,23 +1,20 @@
-export type Svgs = keyof typeof svgs;
+export type Svgs =
+  | "ArrowOutward"
+  | "Behance"
+  | "BeproNetwork"
+  | "Cross"
+  | "GitHub"
+  | "Language"
+  | "LinkedIn"
+  | "Medium"
+  | "Pen"
+  | "Polkamarkets"
+  | "Ufal"
+  | "Vitalk";
+type ImportSvg = typeof import("*.svg");
 
 export default async function getIconSvg(svg: Svgs) {
-  const Svg = await svgs[svg]();
+  const Svg = (await import(`public/icons/${svg}.svg`)) as ImportSvg;
 
   return Svg.ReactComponent;
 }
-
-// Pairing each callable splitted SVG with its key to turn the usage predictable
-const svgs = {
-  ArrowOutward: () => import("./svgs/ArrowOutward.svg"),
-  Behance: () => import("./svgs/Behance.svg"),
-  BeproNetwork: () => import("./svgs/BeproNetwork.svg"),
-  Cross: () => import("./svgs/Cross.svg"),
-  GitHub: () => import("./svgs/GitHub.svg"),
-  Language: () => import("./svgs/Language.svg"),
-  LinkedIn: () => import("./svgs/LinkedIn.svg"),
-  Medium: () => import("./svgs/Medium.svg"),
-  Pen: () => import("./svgs/Pen.svg"),
-  Polkamarkets: () => import("./svgs/Polkamarkets.svg"),
-  Ufal: () => import("./svgs/Ufal.svg"),
-  Vitalk: () => import("./svgs/Vitalk.svg"),
-};
