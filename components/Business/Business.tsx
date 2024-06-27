a91b45f9916e12b1eaae9113a0a9fb39fddba6b6
Victor <soujvnunes@gmail.com>
2024-06-27 15:06:49 +0100

    feat(Business) add heading

--- components/Business/Business.tsx
+++ components/Business/Business.tsx
@@ -5,13 +5,19 @@ import BusinessSocialsSkeleton from "./BusinessSocialsSkeleton";
 
 export default function Business() {
   return (
-    <section>
+    <section className="space-y-md">
+      <h1 className="text-center">
+        <span className="TypographyHeading block">John Victor</span>
+        <span className="TypographyCaption TypographySecondary">
+          software engineer
+        </span>
+      </h1>
       <Suspense fallback={<BusinessSocialsSkeleton />}>
         <BusinessSocials />
       </Suspense>
       {process.env.GDOCS_CV && (
         <Link
-          className="Button"
+          className="Button ButtonMd ButtonMain"
           target="_blank"
           rel="noreferrer noopener"
           href={process.env.GDOCS_CV}
