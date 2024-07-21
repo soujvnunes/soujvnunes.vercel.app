18494c8cb036ef56aa6c64f05c4f0bec7fa0f005
Victor <soujvnunes@gmail.com>
2024-07-21 15:14:25 +0100

    feat(actions/getData) remove unecessary splitted modules

--- actions/getData.ts
+++ actions/getData.ts
@@ -1,11 +1,9 @@
-import { type RawDataProps } from "./getRawData";
+import buildData from "lib/buildData";
+import getRawData, { type RawDataProps } from "./getRawData";
 
 export default async function getData<O extends object = object>(
   props: RawDataProps,
 ) {
-  const { default: getRawData } = await import("actions/getRawData");
-  const { default: buildData } = await import("lib/buildData");
-
   const rawData = await getRawData(props);
   const data = buildData<O>(rawData);
 
