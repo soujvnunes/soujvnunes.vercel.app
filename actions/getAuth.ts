9f0d33874f7204604fd9eb10a1528bf305a3a020
Victor <soujvnunes@gmail.com>
2024-07-21 15:13:10 +0100

    feat(actions/getAuth) move type and domain to env

--- actions/getAuth.ts
+++ actions/getAuth.ts
@@ -1,21 +1,23 @@
-export default async function getAuth() {
-  const { google } = await import("googleapis");
+import { cache } from "react";
+import { google } from "googleapis";
+import formatNewLine from "lib/formatNewLine";
 
+async function getAuth() {
+  const privateKey = formatNewLine(process.env.GAPI_PRIVATE_KEY || "");
   const client = await google.auth.getClient({
     projectId: process.env.GAPI_PROJECT_ID,
     credentials: {
-      type: "service_account",
+      type: process.env.GAPI_TYPE,
       project_id: process.env.GAPI_PROJECT_ID,
       private_key_id: process.env.GAPI_PRIVATE_KEY_ID,
-      private_key: process.env.GAPI_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      private_key: privateKey,
       client_email: process.env.GAPI_CLIENT_EMAIL,
-      universe_domain: "googleapis.com",
+      universe_domain: process.env.GAPI_UNIVERSAL_DOMAIN,
     },
     scopes: [process.env.GAPI_SHEET_SCOPE ?? ""],
   });
 
-  return {
-    api: google,
-    client,
-  };
+  return client;
 }
+
+export default cache(getAuth);
