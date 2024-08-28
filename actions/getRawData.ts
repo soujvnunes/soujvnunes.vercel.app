fcf4399bc338ed695b515dc87a9915008add50d1
Victor <soujvnunes@gmail.com>
2024-08-28 16:01:15 +0100

    feat(actions/getRawData) remove unecessary splitted modules

--- actions/getRawData.ts
+++ actions/getRawData.ts
@@ -9,17 +9,21 @@ export type RawData = string[][] | null | undefined;
 export default async function getRawData(
   props: RawDataProps,
 ): Promise<RawData> {
-  const auth = await getAuth();
+  try {
+    const auth = await getAuth();
 
-  const googleSheets = google.sheets({
-    version: "v4",
-    auth,
-  });
+    const googleSheets = google.sheets({
+      version: "v4",
+      auth,
+    });
 
-  const googleSheet = await googleSheets.spreadsheets.values.get({
-    spreadsheetId: process.env.GAPI_SHEET_ID,
-    range: props.id,
-  });
+    const googleSheet = await googleSheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GAPI_SHEET_ID,
+      range: props.id,
+    });
 
-  return googleSheet.data.values;
+    return googleSheet.data.values;
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
 }
