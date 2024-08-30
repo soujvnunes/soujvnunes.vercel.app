11f1b87b1d1da500ff1c1c4f3f4c17a515d99f9d
Victor <soujvnunes@gmail.com>
2024-08-30 20:42:14 +0100

    feat(lib/getIconSvg) move to actions

---
+++ actions/getSvg.ts
@@ -0,0 +1,24 @@
+export type Svgs =
+  | "ArrowOutward"
+  | "Behance"
+  | "BeproNetwork"
+  | "Cross"
+  | "GitHub"
+  | "Language"
+  | "LinkedIn"
+  | "Medium"
+  | "Pen"
+  | "Polkamarkets"
+  | "Ufal"
+  | "Vitalk";
+type Svg = typeof import("*.svg");
+
+export default async function getSvg(svg: Svgs) {
+  try {
+    const Svg = (await import(`public/icons/${svg}.svg`)) as Svg;
+
+    return Svg.ReactComponent;
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+}
