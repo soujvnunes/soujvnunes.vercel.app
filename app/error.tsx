a69f6afeb9e5fe955c6a645fe04e0f4ef633b1ae
Victor <soujvnunes@gmail.com>
2024-09-29 21:35:01 +0100

    feat(app/error) adjust next error props

--- app/error.tsx
+++ app/error.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { twText } from "lib/ui";
-import { twMerge } from "tailwind-merge";
-
-interface ErrorProps {
-  reset(): void;
-  error: Error & {
-    digest?: string;
-  };
-}
+import { type ErrorProps } from "next";
 
 export default function Error({ error, reset }: ErrorProps) {
   return (
     <section className="m-auto text-center">
-      <h2 className={twMerge(twText.title, "mb-md")}>{error.message}</h2>
+      <h2 className="TypographyTitle mb-md">{error.message}</h2>
       <button onClick={reset}>Try again</button>
     </section>
   );
